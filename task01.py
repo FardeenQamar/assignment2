f7550040e0d1e14a2221a4e5abd47bb52e74c6cd
FardeenQamar <qamarfardeen@gmail.com>
2025-04-11 20:42:01 +0530

    Add files via upload

---
+++ task01.py
@@ -0,0 +1,7 @@
+Number = int(input('Enter a Number : '))
+if (Number / 2) == (Number // 2) :
+    print(f'{Number} is an even number ')
+else:
+    print(f'{Number} is an odd number ')
+
+print('Thank You')
---
+++ task02.py
@@ -0,0 +1,5 @@
+list_of_numbers = range(50)
+num1 = 0
+for num in list_of_numbers:
+    num1 += num
+print(f'The sum of numbers from 1 to 50 is {num1} ')
